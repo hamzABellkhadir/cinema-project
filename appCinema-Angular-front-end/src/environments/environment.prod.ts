ff35516abaf5fdb79247485fb69cf16adfae1d1a
Hamza Bk <hamzabk351@gmail.com>
2021-06-09 11:27:30 +0100

    cinema-project

---
+++ appCinema-Angular-front-end/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase:{
+  apiKey: "AIzaSyBhQtJnNobzwSdzeI21Lwr0ypPZcsxQtcE",
+  authDomain: "cinema-demo-5e12f.firebaseapp.com",
+  databaseURL: "https://cinema-demo-5e12f-default-rtdb.firebaseio.com",
+  projectId: "cinema-demo-5e12f",
+  storageBucket: "cinema-demo-5e12f.appspot.com",
+  messagingSenderId: "802445573209",
+  appId: "1:802445573209:web:7202b96e8e2506a28a4548"
+  }
+};
+/*  */
---
+++ appCinema-Angular-front-end/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+  apiKey: "AIzaSyBhQtJnNobzwSdzeI21Lwr0ypPZcsxQtcE",
+  authDomain: "cinema-demo-5e12f.firebaseapp.com",
+  databaseURL: "https://cinema-demo-5e12f-default-rtdb.firebaseio.com",
+  projectId: "cinema-demo-5e12f",
+  storageBucket: "cinema-demo-5e12f.appspot.com",
+  messagingSenderId: "802445573209",
+  appId: "1:802445573209:web:7202b96e8e2506a28a4548"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
